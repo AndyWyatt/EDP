8fcbe0a342fcd9232f46a978e410fcf4159a0dc5
Andy Wyatt <andy.wyatt@bsdc.ac.uk>
2017-10-18 16:27:11 +0100

    BankAccountNote GUI work

---
+++ EDP25.Bank.WPF/NoteAdd.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EDP25.Bank.WPF
+{
+    /// <summary>
+    /// Interaction logic for NoteAdd.xaml
+    /// </summary>
+    public partial class NoteAdd : Window
+    {
+        public NoteAdd()
+        {
+            InitializeComponent();
+
+            butCancel.Click += ButCancel_Click;
+            butOk.Click += ButOk_Click;
+
+            txtOwnerName.Focus();
+        }
+
+        private void ButOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void ButCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
